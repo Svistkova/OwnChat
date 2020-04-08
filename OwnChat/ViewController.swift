d804630fc127a47b5555186fb53c708098b838e1
Marina Svistkova <svmrin1@gmail.com>
2020-04-08 13:16:34 +0300

    Initial Commit

---
+++ OwnChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OwnChat
+//
+//  Created by Marina Svistkova on 08.04.2020.
+//  Copyright © 2020 Marina Svistkova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
